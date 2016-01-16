66c9e4932cf00bed4a65a35b32ea366be71dfe1e
dkim2 <dkim2@macalester.edu>
2016-01-16 17:39:36 -0600

    Put some work in to account for whether or not the file is .txt or an input string. Might just erase this later and account for input string. Sorted() seems to work differently so I’ll look into that

--- Parser/Parserv1.py
+++ Parser/Parserv1.py
@@ -1,12 +1,36 @@
-#My attempt at a basic Python-based Parser. Will take in text and reorganize based on input.
+#My attempt at a basic Python-based Parser. Will take in ASCII text and reorganize based on input.
 import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("sort", help = "file holder")
+
+#arguments
+parser.add_argument("file", help = "stores file")
+parser.add_argument("--sort", help = "alphabetize", default = False, action = "store_true")
+parser.add_argument("--reverse", "-r", help = "reverses input", default = False, action = "store_true")
+
 args = parser.parse_args()
+if args.file.endswith('.txt'):
+	wordSortList = []
+	with open(args.file) as file:
+	    for line in file:
+        	wordSortList.append(line)
+else:
+	wordSortList = args.file.split()
 
 print("apple")
-with open(args.sort) as file:
-    for line in file:
-        print(line)
+
+
+if args.sort:
+	sorted(wordSortList)
+
+if args.reverse:
+	wordSortList.reverse()
+
+for a in wordSortList:
+	print(a)
+
+
+
+
+
