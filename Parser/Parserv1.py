bcee4b421065179eda72adc2e116663d01b4ad07
dkim2 <dkim2@macalester.edu>
2016-01-15 21:10:30 -0600

    Description added

--- Parser/Parserv1.py
+++ Parser/Parserv1.py
@@ -1,4 +1,6 @@
+#My attempt at a basic Python-based Parser. Will take in text and reorganize based on input.
 import argparse
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("sort", help = "file holder")
